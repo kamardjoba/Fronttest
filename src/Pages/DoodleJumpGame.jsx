55a7ca0453360044488f84d3164ac22b3dd7e5b0
kamardjoba <Nazarlymar152@gmail.com>
2024-10-20 01:14:43 +0200

    initial

--- src/Pages/DoodleJumpGame.jsx
+++ src/Pages/DoodleJumpGame.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import doodlerRightSrc from '../IMG/Doodle_jump/MainCharacter_right.png';
 import doodlerLeftSrc from '../IMG/Doodle_jump/MainCharacter_left.png';
 import platformSrc from '../IMG/Doodle_jump/MainPlatform.png';
@@ -7,8 +7,11 @@ import restartImgSrc from '../IMG/Doodle_jump/Again.png';
 import '../Css/doodlejump.css';
 
 
+
+
 const DoodleJumpGame = () => {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
   const [showRestartButton, setShowRestartButton] = useState(false);
 
   // Размеры и объекты
@@ -17,56 +20,58 @@ const DoodleJumpGame = () => {
 
   let doodlerWidth = 60;
   let doodlerHeight = 60;
-  let doodlerRightImg = new Image();
-  let doodlerLeftImg = new Image();
-  let platformImg = new Image();
 
-  let doodler = {
+  const doodlerRightImg = useRef(new Image());
+  const doodlerLeftImg = useRef(new Image());
+  const platformImg = useRef(new Image());
+
+  const doodler = useRef({
     img: null,
     x: 0,
     y: 0,
     width: doodlerWidth,
     height: doodlerHeight,
-  };
+  });
 
   // Физика
-  let velocityX = 0;
-  let velocityY = 0;
-  let initialVelocityY = -15;
-  let gravity = 0.5;
+  const velocityX = useRef(0);
+  const velocityY = useRef(0);
+  const initialVelocityY = useRef(-15);
+  const gravity = useRef(0.5);
 
   // Платформы
-  let platformArray = [];
+  const platformArray = useRef([]);
   let platformWidth = 85;
   let platformHeight = 20;
 
-  let score = 0;
-  let maxScore = 0;
-  let gameOver = false;
+  const score = useRef(0);
+  const maxScore = useRef(0);
+  const gameOver = useRef(false);
 
   // Загрузка изображений
-  doodlerRightImg.src = doodlerRightSrc;
-  doodlerLeftImg.src = doodlerLeftSrc;
-  platformImg.src = platformSrc;
+  doodlerRightImg.current.src = doodlerRightSrc;
+  doodlerLeftImg.current.src = doodlerLeftSrc;
+  platformImg.current.src = platformSrc;
 
-  doodler.img = doodlerRightImg;
+  doodler.current.img = doodlerRightImg.current;
 
   // Фоновое изображение
-  const backgroundImg = new Image();
-  backgroundImg.src = backgroundSrc;
+  const backgroundImg = useRef(new Image());
+  backgroundImg.current.src = backgroundSrc;
 
   useEffect(() => {
     // Инициализация Canvas
     const board = canvasRef.current;
     const context = board.getContext('2d');
+    contextRef.current = context; // Сохраняем контекст в ref
     board.width = boardWidth;
     board.height = boardHeight;
 
-    backgroundImg.onload = function () {
-      context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
+    backgroundImg.current.onload = function () {
+      context.drawImage(backgroundImg.current, 0, 0, boardWidth, boardHeight);
     };
 
-    velocityY = initialVelocityY;
+    velocityY.current = initialVelocityY.current;
     placePlatforms();
     requestAnimationFrame(update);
 
@@ -78,7 +83,7 @@ const DoodleJumpGame = () => {
       board.height = boardHeight;
 
       // Перерисовываем фон
-      context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
+      contextRef.current.drawImage(backgroundImg.current, 0, 0, boardWidth, boardHeight);
 
       // Переразмещаем платформы
       placePlatforms();
@@ -90,170 +95,179 @@ const DoodleJumpGame = () => {
     return () => {
       window.removeEventListener('resize', resizeCanvas);
     };
+  }, []);
 
-    // Функция обновления игры
-    function update() {
-      if (gameOver) {
-        setShowRestartButton(true); // Показываем кнопку перезапуска
-        return;
-      }
-      context.clearRect(0, 0, board.width, board.height);
+  // Функция обновления игры
+  function update() {
+    if (gameOver.current) {
+      setShowRestartButton(true); // Показываем кнопку перезапуска
+      return;
+    }
+    contextRef.current.clearRect(0, 0, boardWidth, boardHeight);
 
-      // Отрисовка фона
-      context.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight);
+    // Отрисовка фона
+    contextRef.current.drawImage(backgroundImg.current, 0, 0, boardWidth, boardHeight);
 
-      // Обновление позиции дудлера
-      doodler.x += velocityX;
-      if (doodler.x > boardWidth) {
-        doodler.x = 0 - doodler.width;
-      } else if (doodler.x + doodler.width < 0) {
-        doodler.x = boardWidth;
-      }
-
-      velocityY += gravity;
-      doodler.y += velocityY;
-      if (doodler.y > board.height) {
-        gameOver = true;
-      }
-      context.drawImage(
-        doodler.img,
-        doodler.x,
-        doodler.y,
-        doodler.width,
-        doodler.height
-      );
-
-      // Обновление платформ
-      for (let i = 0; i < platformArray.length; i++) {
-        let platform = platformArray[i];
-        if (velocityY < 0 && doodler.y < boardHeight * 0.75) {
-          platform.y -= initialVelocityY;
-        }
-        if (detectCollision(doodler, platform) && velocityY >= 0) {
-          velocityY = initialVelocityY;
-        }
-        context.drawImage(
-          platform.img,
-          platform.x,
-          platform.y,
-          platform.width,
-          platform.height
-        );
-      }
-
-      // Удаление старых платформ и добавление новых
-      while (platformArray.length > 0 && platformArray[0].y >= boardHeight) {
-        platformArray.shift();
-        newPlatform();
-      }
-
-      // Обновление счета
-      updateScore();
-      context.fillStyle = 'black';
-      context.font = `20px sans-serif`;
-      context.fillText(score, 5, 20);
-
-      requestAnimationFrame(update);
+    // Обновление позиции дудлера
+    doodler.current.x += velocityX.current;
+    if (doodler.current.x > boardWidth) {
+      doodler.current.x = 0 - doodler.current.width;
+    } else if (doodler.current.x + doodler.current.width < 0) {
+      doodler.current.x = boardWidth;
     }
 
-    // Функция размещения платформ
-    function placePlatforms() {
-      platformArray = [];
+    velocityY.current += gravity.current;
+    doodler.current.y += velocityY.current;
+    if (doodler.current.y > boardHeight) {
+      gameOver.current = true;
+    }
+    contextRef.current.drawImage(
+      doodler.current.img,
+      doodler.current.x,
+      doodler.current.y,
+      doodler.current.width,
+      doodler.current.height
+    );
 
-      const gapBetweenPlatforms = 100;
-      const numberOfPlatforms = Math.ceil(
-        boardHeight / (platformHeight + gapBetweenPlatforms)
-      );
-
-      for (let i = 0; i < numberOfPlatforms; i++) {
-        let randomX = Math.floor(Math.random() * (boardWidth - platformWidth));
-
-        let platform = {
-          img: platformImg,
-          x: randomX,
-          y:
-            boardHeight -
-            i * (platformHeight + gapBetweenPlatforms) -
-            platformHeight,
-          width: platformWidth,
-          height: platformHeight,
-        };
-
-        platformArray.push(platform);
+    // Обновление платформ
+    for (let i = 0; i < platformArray.current.length; i++) {
+      let platform = platformArray.current[i];
+      if (
+        velocityY.current < 0 &&
+        doodler.current.y < boardHeight * 0.75
+      ) {
+        platform.y -= initialVelocityY.current;
       }
-
-      // Устанавливаем дудлера на первую платформу
-      let firstPlatform = platformArray[0];
-      doodler.x =
-        firstPlatform.x + firstPlatform.width / 2 - doodler.width / 2;
-      doodler.y = firstPlatform.y - doodler.height;
+      if (
+        detectCollision(doodler.current, platform) &&
+        velocityY.current >= 0
+      ) {
+        velocityY.current = initialVelocityY.current;
+      }
+      contextRef.current.drawImage(
+        platform.img,
+        platform.x,
+        platform.y,
+        platform.width,
+        platform.height
+      );
     }
 
-    // Функция создания новой платформы
-    function newPlatform() {
+    // Удаление старых платформ и добавление новых
+    while (
+      platformArray.current.length > 0 &&
+      platformArray.current[0].y >= boardHeight
+    ) {
+      platformArray.current.shift();
+      newPlatform();
+    }
+
+    // Обновление счета
+    updateScore();
+    contextRef.current.fillStyle = 'black';
+    contextRef.current.font = `20px sans-serif`;
+    contextRef.current.fillText(score.current, 5, 20);
+
+    requestAnimationFrame(update);
+  }
+
+  // Функция размещения платформ
+  function placePlatforms() {
+    platformArray.current = [];
+
+    const gapBetweenPlatforms = 100;
+    const numberOfPlatforms = Math.ceil(
+      boardHeight / (platformHeight + gapBetweenPlatforms)
+    );
+
+    for (let i = 0; i < numberOfPlatforms; i++) {
       let randomX = Math.floor(Math.random() * (boardWidth - platformWidth));
+
       let platform = {
-        img: platformImg,
+        img: platformImg.current,
         x: randomX,
-        y: -platformHeight,
+        y:
+          boardHeight -
+          i * (platformHeight + gapBetweenPlatforms) -
+          platformHeight,
         width: platformWidth,
         height: platformHeight,
       };
 
-      platformArray.push(platform);
+      platformArray.current.push(platform);
     }
 
-    // Функция обнаружения столкновения
-    function detectCollision(a, b) {
-      return (
-        a.x < b.x + b.width &&
-        a.x + a.width > b.x &&
-        a.y < b.y + b.height &&
-        a.y + a.height > b.y
-      );
-    }
+    // Устанавливаем дудлера на первую платформу
+    let firstPlatform = platformArray.current[0];
+    doodler.current.x =
+      firstPlatform.x + firstPlatform.width / 2 - doodler.current.width / 2;
+    doodler.current.y = firstPlatform.y - doodler.current.height;
+  }
 
-    // Функция обновления счета
-    function updateScore() {
-      let points = Math.floor(50 * Math.random());
-      if (velocityY < 0) {
-        maxScore += points;
-        if (score < maxScore) {
-          score = maxScore;
-        }
-      } else if (velocityY >= 0) {
-        maxScore -= points;
+  // Функция создания новой платформы
+  function newPlatform() {
+    let randomX = Math.floor(Math.random() * (boardWidth - platformWidth));
+    let platform = {
+      img: platformImg.current,
+      x: randomX,
+      y: -platformHeight,
+      width: platformWidth,
+      height: platformHeight,
+    };
+
+    platformArray.current.push(platform);
+  }
+
+  // Функция обнаружения столкновения
+  function detectCollision(a, b) {
+    return (
+      a.x < b.x + b.width &&
+      a.x + a.width > b.x &&
+      a.y < b.y + b.height &&
+      a.y + a.height > b.y
+    );
+  }
+
+  // Функция обновления счета
+  function updateScore() {
+    let points = Math.floor(50 * Math.random());
+    if (velocityY.current < 0) {
+      maxScore.current += points;
+      if (score.current < maxScore.current) {
+        score.current = maxScore.current;
       }
+    } else if (velocityY.current >= 0) {
+      maxScore.current -= points;
     }
-  }, []);
+  }
 
   // Функции управления
   function moveLeft(e) {
     e.preventDefault();
-    if (gameOver) return;
-    velocityX = -6;
-    doodler.img = doodlerLeftImg;
+    if (gameOver.current) return;
+    velocityX.current = -6;
+    doodler.current.img = doodlerLeftImg.current;
   }
 
   function moveRight(e) {
     e.preventDefault();
-    if (gameOver) return;
-    velocityX = 6;
-    doodler.img = doodlerRightImg;
+    if (gameOver.current) return;
+    velocityX.current = 6;
+    doodler.current.img = doodlerRightImg.current;
   }
 
   function stopMoving(e) {
     e.preventDefault();
-    if (gameOver) return;
-    velocityX = 0;
+    if (gameOver.current) return;
+    velocityX.current = 0;
   }
 
   function resetGame() {
-    velocityX = 0;
-    velocityY = initialVelocityY;
-    score = 0;
-    maxScore = 0;
-    gameOver = false;
+    velocityX.current = 0;
+    velocityY.current = initialVelocityY.current;
+    score.current = 0;
+    maxScore.current = 0;
+    gameOver.current = false;
     placePlatforms();
     setShowRestartButton(false);
     requestAnimationFrame(update);
